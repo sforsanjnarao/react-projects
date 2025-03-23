1a0bd139c7fbe2bec99ed81a86b59e67a16433d6
sforsanjnarao <sanjnarao26@gmail.com>
2025-03-24 04:13:30 +0530

    toggel with context api

--- themeChanger/src/App.jsx
+++ themeChanger/src/App.jsx
@@ -1,9 +1,23 @@
  import React from 'react'
- 
+import { useState } from 'react'
+import { createContext } from 'react'
+import ChildA from './component/ChildA'
+import './App.css'
+const themeContext=createContext()
+
  function App() {
+  const [theme, setTheme]=useState('light')
    return (
-     <div>App</div>
+    <themeContext.Provider value={{theme,setTheme}}>
+      <div id='container' style={{backgroundColor:theme==='light'?'beige':'gray'}}>
+      <ChildA />
+      </div>
+      
+    </themeContext.Provider >
+
+    
    )
  }
  
  export default App
+ export { themeContext }
--- themeChanger/src/component/ChildC.jsx
+++ themeChanger/src/component/ChildC.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import { useContext } from 'react'
+import { createContext } from 'react'
+import { themeContext } from '../App'
+
+
 
 function ChildC() {
+    const {theme,setTheme}=useContext(themeContext)
+
+    const toggleTheme=()=>{
+        setTheme(theme==='light'?'dark':'light')
+    }
   return (
-    <div>it's childC</div>
+    <div>
+        <button onClick={toggleTheme}>
+             toggle
+        </button>
+    </div>
   )
 }
 
